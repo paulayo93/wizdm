96f9228e23f4ffd1379b7bbe5876fca28638479a
Paul Ayo Oloyede <ayopaulofficial@gmail.com>
2020-09-07 05:14:51 +0100

    add new url

--- wizdm/src/app/pages/explore/feed/add-post/add-post.module.ts
+++ wizdm/src/app/pages/explore/feed/add-post/add-post.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddPostComponent } from './add-post.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AvatarModule } from '@wizdm/elements/avatar';
+import { IconModule } from '@wizdm/elements/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+import {MatListModule} from '@angular/material/list'; 
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        FlexLayoutModule,
+        MatButtonModule,
+        MatCardModule,
+        AvatarModule,
+        IconModule,
+        MatListModule
+    ],
     exports: [AddPostComponent],
     declarations: [AddPostComponent],
 })
