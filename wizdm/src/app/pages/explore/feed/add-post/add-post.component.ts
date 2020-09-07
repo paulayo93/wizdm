10714554972cb3f010f2e4bbaa4675338f33490e
Paul Oloyede <Paul Oloyede>
2020-09-07 19:15:06 +0100

    set card for post

--- wizdm/src/app/pages/explore/feed/add-post/add-post.component.ts
+++ wizdm/src/app/pages/explore/feed/add-post/add-post.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'wm-addpost',
     templateUrl: 'add-post.component.html',
-    styleUrls: ['add-post.component.scss']
+    styleUrls: ['add-post.component.scss'],
+    host: { 'class': 'wm-addpost' },
 })
 
 export class AddPostComponent implements OnInit {
